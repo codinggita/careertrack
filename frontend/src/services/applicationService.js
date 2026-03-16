4c62af0afe320d59b15e7e5bf3503c709c7bc0b6
Hetavi <hetavipanchotia@gmail.com>
2026-03-16 15:28:22 +0530

    changes to deploy smoothly

--- frontend/src/services/applicationService.js
+++ frontend/src/services/applicationService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/applications';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/applications`;
 
 /**
  * Fetch all applications for a specific user with filtering/pagination.
