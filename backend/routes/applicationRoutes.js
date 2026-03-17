7de95b1d271cf0427aa1a455b8d2d89d96baeef8
Hetavi <hetavipanchotia@gmail.com>
2026-03-17 14:23:57 +0530

    mongodb atlas

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {
-  createApplication,
-  getApplications,
-  getInterviews,
-  getUpcomingInterviews,
-  updateApplication,
-  deleteApplication
+const { 
+  createApplication, 
+  getApplications, 
+  getInterviews, 
+  getUpcomingInterviews, 
+  updateApplication, 
+  deleteApplication 
 } = require('../controllers/applicationController');
 
-// All routes are mounted under /api/applications in server.js
 router.post('/', createApplication);
 router.get('/', getApplications);
 router.get('/interviews', getInterviews);
